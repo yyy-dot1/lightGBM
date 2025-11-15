a76e23ec95374d26f17e7c552aa3ef74d11e476c
田中結那（たなかゆいな） <yuina.tnk@gmail.com>
2025-11-16 05:30:21 +0900

    fixed

---
+++ my_project/main.py
@@ -0,0 +1,82 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, confusion_matrix
+import lightgbm as lgb
+import mysql.connector
+
+
+#データの読み込み
+csv_file = "sample.csv"
+df = pd.read_csv(csv_file)
+
+# MySQLに接続
+conn = mysql.connector.connect(
+    host="127.0.0.1",
+    port=3307,          # Dockerでマッピングしたポート
+    user="root",        # ユーザー名
+    password="dgVH61",  # Docker起動時に設定したパスワード
+    database="mydb"
+)
+
+cursor = conn.cursor()
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS sample (
+    ブログ更新 INT,
+    試験勉強 INT,
+    コーディング INT,
+    環境構築 INT,
+    読書 INT,
+    `1日の幸福度` INT
+)
+""")
+
+for _, row in df.iterrows():
+    cursor.execute("""
+    INSERT INTO sample (ブログ更新, 試験勉強, コーディング, 環境構築, 読書, `1日の幸福度`)
+    VALUES (%s, %s, %s, %s, %s, %s)
+    """, tuple(row))
+
+conn.commit()
+
+query = "SELECT * FROM sample LIMIT 20"
+df_limited = pd.read_sql(query,conn)
+print(df_limited)
+
+# #予測ターゲットの格納
+target_df = df[["1日の幸福度"]]
+
+# #特徴量の格納（説明変数：B列以降）
+train_df = df[["ブログ更新","試験勉強", "コーディング", "環境構築", "読書"]]
+
+# #モデル学習のための、訓練データとテストデータを7:3で分割
+X_train, X_test, y_train, y_test = train_test_split(train_df, target_df, test_size=0.3)
+
+# #XGBoostで学習するためのデータ形式に変換
+train_data = lgb.Dataset(X_train, label=y_train)
+test_data = lgb.Dataset(X_test, label=y_test, reference=train_data)
+
+# ハイパーパラメータ設定
+params = {
+    'objective': 'binary',
+    'metric': 'binary_error',
+    'boosting_type': 'gbdt',
+    'learning_rate': 0.1,
+    'num_leaves': 31,
+    'verbose': -1
+}
+
+# モデルパラメータ設定
+model = lgb.train(params, train_data, valid_sets=[test_data], num_boost_round=100)
+
+# 予測
+y_pred_prob = model.predict(X_test)
+y_pred = [1 if p > 0.5 else 0 for p in y_pred_prob]
+
+accuracy = accuracy_score(y_test, y_pred)
+cm = confusion_matrix(y_test, y_pred)
+
+# モデルが正しく予測できたデータの割合
+print("Accuracy:", accuracy)
+#混同行列
+print("Confusion Matrix:\n", cm)
---
+++ my_project/practice.py
@@ -0,0 +1,62 @@
+import pandas as pd
+
+df = pd.read_csv("sample.csv")
+
+#先頭5行
+# print(df.head())
+#行数・列数
+# print(df.shape)
+#列名
+# print(df.columns)
+#データの概要
+# print(df.info())
+#統計量
+# print(df.describe())
+#単列
+# print(df["読書"])
+#複数列
+# print(df[["読書","コーディング"]])
+#0番目の行
+# print(df.loc[0])
+#行番号で複数行
+# print(df.loc[0:3])
+#読書が1の行だけ
+# print(df[df["読書"] == 1])
+# print(df[df["読書"] != 1])
+#Not条件(~以外)
+# print(df[~(df["読書"] == 1)])
+#読書=1勝コーディング=1
+# print(df[(df["読書"] == 1) & (df["コーディング"] == 1)])
+# print(df[(df["読書"] == 1) | (df["コーディング"] == 1)])
+# 列の追加
+# df["新しい列"] = 1
+# print(df.head())
+# #計算で作る列
+# df["合計"] = df["読書"] + df["コーディング"]
+# 欠損値の有無確認
+# print(df["合計"])
+# print(df.isna().sum())
+#欠損を埋める
+# print(df.fillna(0))
+# csvに保存
+# df.to_csv("output.csv",index=False)
+# print(df.sort_values("読書",ascending=False))
+# print(df.head(2))
+#さらに条件で絞る
+# print(df["コーディング"] + df["読書"] > 1)
+#group by
+# print(df.groupby("読書").mean())
+# print(df.groupby("読書")["コーディング"].mean())
+# print(df["読書"].sum())
+# print(df["読書"].mean())
+# print(df["読書"].count())
+#重複削除
+# print(df["読書"].unique())
+# df.drop_duplicates()
+#MYSQLのデータ削除
+# cursor.execute("TRUNCATE sample")
+# conn.commit()
+
+
+# df[df["点数"].between(60, 80)]
+# df[df["A"].isin([1, 5, 9])]
